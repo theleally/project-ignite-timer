ae6a28b7644801786224f70203ce1b42199b692b
Leandro Allysson <64550528+theleally@users.noreply.github.com>
2024-12-21 11:12:43 -0300

    setting font

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -20,7 +20,8 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border: 0;
   margin: 8px;
 
-  background-color: ${ props => props.theme.primary };
+  color: ${props => props.theme.white};
+  background-color: ${ props => props.theme["green-500"] };
 
   /* ${props => {
     return css`
